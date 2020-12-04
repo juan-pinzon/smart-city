b7de4cf1fc75f9b1d3b67d1e28816f6ce9769c24
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-12-04 14:37:53 -0500

    config env for prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   url_base: 'http://4bd428738c00.ngrok.io/api/',
+  endPoints: {
+    devices: {
+      list: 'devices'
+    },
+    measurements: {
+      list: 'measurements',
+      graphic: 'measurements/graphic'
+    }
+  }
 };
