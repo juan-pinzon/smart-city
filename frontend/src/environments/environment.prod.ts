64041dec106ab819b80afde4ed2092325f1c10aa
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-12-04 15:29:17 -0500

    endPint from heroku

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url_base: 'https://4bd428738c00.ngrok.io/api/',
+  url_base: 'https://smart-city-uao.herokuapp.com/api/',
   endPoints: {
     devices: {
       list: 'devices'
